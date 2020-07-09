0e1b80a60ec36b4f10db398da48917b0e42aaf07
Shreyas <svader231@gmail.com>
2020-07-09 16:05:30 -0400

    Added bot capability to send email to in stock and out of stock items

--- venv/main.py
+++ venv/main.py
@@ -6,7 +6,13 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.chrome.options import Options
 import collections
+from sendMail import sendM
+from receiveMail import receiveM
+#Take out privateInfo and input your own private information
+import privateInfo
+import json
 
 class itemTracker:
     items = []
@@ -17,9 +23,17 @@ class itemTracker:
         self.username = username
         self.password = password
         self.items = items
+        chrome_options = Options()
+        #Change information to your respective file destinations
+        chrome_options.add_argument("--user-data-dir=C:\\Users\\Shreyas\\AppData\\Local\\Google\\Chrome\\User")
+        chrome_options.add_argument('--profile-directory=Profile 1')
         self.driver = webdriver.Chrome(r'C:\Users\Shreyas\Downloads\chromedriver_win32\chromedriver.exe')
+
     def logIn(self):
         self.driver.get('https://www.amazon.com/')
+        if len(self.items) ==0:
+            self.driver.close()
+            raise Exception("No items to check")
         try:
             element = WebDriverWait(self.driver, 10).until(
                 expected_conditions.presence_of_element_located((By.XPATH, "/html/body"))
@@ -48,6 +62,9 @@ class itemTracker:
         self.driver.find_element_by_xpath('//*[@id="signInSubmit"]').click()
         sleep(15)
     def itemAvailability(self):
+        if len(self.items) ==0:
+            self.driver.close()
+            raise Exception("No items to check")
         for item in self.items:
             self.driver.get(item)
             try:
@@ -64,6 +81,8 @@ class itemTracker:
             else:
                 self.notInStock[item] = stock
     def addToCart(self):
+        with open('list.txt', "r") as file:
+            filedata = file.read()
         for item in self.inStock:
             self.driver.get(item)
             try:
@@ -75,17 +94,16 @@ class itemTracker:
                 self.driver.quit()
             try:
                 self.driver.find_element_by_xpath('//*[@id="add-to-cart-button"]').click()
-                
             except:
                 print("Unable to add "+self.driver.find_element_by_xpath('//*[@id="productTitle"]').text)
+            if item in self.items:
+                self.items.remove(item)
+            filedata = filedata.replace(item, "")
+            with open('list.txt', "w") as file:
+                file.write(filedata)
 
-
-with open('list.txt', 'r') as fh:
-    for line in fh:
-        line = line.rstrip("\n")
 file = open('list.txt','r')
 items = file.readlines()
-print(items)
 tempItems =[]
 for item in items:
     tempItems.append(item)
@@ -93,10 +111,13 @@ x = True
 
 while x:
     item = input("Enter your Item to the List and type 'done' when done: ")
-    items.append("%s"%item)
-    if item == 'done':
-        items.remove('done')
-        x = False
+    if item[0] =='h' or item =='done':
+        items.append("%s"%item)
+        if item =='done':
+            items.remove('done')
+            x = False
+    else:
+        continue
 
 compare = lambda x, y: collections.Counter(x) == collections.Counter(y)
 
@@ -108,9 +129,19 @@ if compare(tempItems,items)==False:
 file.close()
 
 items[:] = [x for x in items if x != "\n"]
-
-tracker = itemTracker("shreeisno1@gmail.com","awedragon",items)
-tracker.logIn()
+print(items)
+#Input username and password
+tracker = itemTracker(privateInfo.amazonUsername,privateInfo.amazonPassword,items)
+#Unable to log in due to Amazon bot protection
+#tracker.logIn()
 tracker.itemAvailability()
 tracker.addToCart()
 
+
+tracker.driver.close()
+
+send = sendM("These items were in Stock: "+str(tracker.inStock)+" while these were out of stock: "
++str(tracker.notInStock))
+send.sending()
+
+tracker.inStock.clear()
---
+++ venv/sendMail.py
@@ -0,0 +1,32 @@
+import smtplib
+from email.mime.text import MIMEText
+#Take out privateInfo and input your own private information
+import privateInfo
+mailFrom = privateInfo.amazonUsername
+mailTo = [privateInfo.botUsername]
+smtp_ssl_host = 'smtp-pulse.com'
+smtp_ssl_port = 587
+#email username
+username = privateInfo.botUsername
+password = privateInfo.botPassword
+
+class sendM:
+    mailSubject = 'Amazon Items'
+    def __init__(self,message):
+        self.message = message
+    def sending(self):
+        message = MIMEText(self.message)
+        message['subject'] = 'Amazon Item Bot'
+        message['from'] = mailFrom
+        message['to'] = ', '.join(mailTo)
+
+
+        message = 'Subject: {}\n\n{}'.format('Amazon Item Bot',self.message)
+        server = smtplib.SMTP('smtp.gmail.com:587')
+        server.ehlo()
+        server.starttls()
+        server.login(username,password)
+        server.sendmail(username,username,message)
+        server.quit()
+
+
